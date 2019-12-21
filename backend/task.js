6b8a44fa644eb123be5426adde11f3d04a3a7f21
wusong <wusong@kingsoft.com>
2019-12-21 14:29:30 +0800

    update

--- backend/task.js
+++ backend/task.js
@@ -2,7 +2,7 @@ const schedule = require('node-schedule');
 const main = require('./main');
 
 const  task = ()=>{
-    schedule .scheduleJob('30 * * * * *',()=>{
+    schedule .scheduleJob('30 1 * * * *',()=>{
         console.log('-----------定时爬虫任务:' + new Date());
         main.run();
     });
