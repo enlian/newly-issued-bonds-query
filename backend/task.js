8ef2c810d4b7ee7c798c5ead26b9eaf4882d3ace
wusong <wusong@kingsoft.com>
2019-12-25 14:48:57 +0800

    update

--- backend/task.js
+++ backend/task.js
@@ -2,7 +2,7 @@ const schedule = require('node-schedule');
 const main = require('./main');
 
 const rule = new schedule.RecurrenceRule();
-rule.minute = [0, 15, 45];
+rule.minute = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
 
 const task = ()=>{
     schedule.scheduleJob(rule,()=>{
